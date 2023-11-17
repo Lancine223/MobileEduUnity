3e7d2c7f52a91a2459eef30431706e122825f42f
Lancine223 <lacinekeita20@gmail.com>
2023-11-17 18:35:50 +0000

    latest

--- src/app/inscriptionetudiant/inscriptionetudiant.component.ts
+++ src/app/inscriptionetudiant/inscriptionetudiant.component.ts
@@ -1,4 +1,16 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
+import { AlertController } from '@ionic/angular';
+import { Classe } from '../model/classe';
+import { Filiere } from '../model/filiere';
+import { Niveau } from '../model/niveau';
+import { AuthService } from '../service/auth.service';
+import { ClasseService } from '../service/classe.service';
+import { FiliereService } from '../service/filiere.service';
+import { NiveauService } from '../service/niveau.service';
+import { EtudiantService } from '../service/etudiant.service';
+import { Etudiant } from '../model/etudiant';
 
 @Component({
   selector: 'app-inscriptionetudiant',
@@ -7,8 +19,194 @@ import { Component, OnInit } from '@angular/core';
 })
 export class InscriptionetudiantComponent  implements OnInit {
 
-  constructor() { }
+  etudiantForm!: FormGroup |any;
+  etudiantRecup: Etudiant | undefined;
+  dataEtudiant : any;
+  niveaux: Niveau[]|any;
+  filieres: Filiere[]|any;
+  classes: Classe[]|any;
 
-  ngOnInit() {}
+
+  constructor(private formBuilder: FormBuilder,
+    // private snack : CoreService,
+    private route: Router,
+    private etudiantService: EtudiantService,
+     private authService: AuthService,
+     private alertController: AlertController,
+     private niveauService: NiveauService,
+     private filieresService: FiliereService,
+     private classesService: ClasseService
+     ) {
+      // this.niveaux[0]= this.niveau1;
+
+    // this.enseignantRecup = this.authService.getEnseignantConnect();
+    // console.log("Admin recup dans guide ", this.adminRecup);
+
+    this.etudiantForm = this.formBuilder.group({
+      nom: ['', Validators.required],
+      prenom: ['', Validators.required],
+      age: [ , Validators.required],
+      telephone: [ , Validators.required],
+      motDePasse: ['', Validators.required],
+      niveau: ['', Validators.required],
+      filiere: ['', Validators.required],
+      classe: ['', Validators.required],
+  });
+
+  }
+
+  ngOnInit(): void {
+    this.loadNiveauList();
+    this.loadFilieres();
+    this.loadClasses();
+    // this.loadFilieres(this.niveau1.idNiveau);
+  }
+
+
+
+
+
+
+        ////////////////////
+
+  async creerEtudiant() {
+    if(this.etudiantForm.valid){
+      const etudiant = this.etudiantForm.value
+      // Obtenez l'élément input par son ID
+            const monInput: HTMLInputElement | null = document.getElementById('repeter') as HTMLInputElement;
+            const valeurInput: string = monInput.value;
+            // Vérifiez si l'élément a été trouvé
+              // Obtenez la valeur de l'élément
+
+
+              // Faites quelque chose avec la valeur
+              console.log('La valeur de mon input est :', valeurInput);
+
+
+      if(valeurInput !== etudiant.motDePasse){
+        this.motdepasseinval();
+      }else{
+
+        this.etudiantService.creerEtudiant(etudiant).subscribe(
+           (response: any) => {
+
+           this.route.navigate(['/student']);
+          this.afficherSucces();
+          },
+           (error:any) => {
+            // Handle login error
+            // console.log("1",error.error.message.text);
+            const errorMessage = JSON.parse(error.error).message;
+
+            this.afficherErreur(errorMessage);
+
+          }
+        );
+
+    }
+    }else{
+      this.afficherRemplir();
+    }
+  }
+
+
+
+
+
+
+  async afficherErreur(message: string) {
+    const alert = await this.alertController.create({
+      header: 'Erreur',
+      message: message,
+      buttons: ['OK']
+    });
+
+    await alert.present();
+  }
+
+  async afficherSucces() {
+    const alert = await this.alertController.create({
+      header: 'Succès',
+      message: 'Votre compte a été créé avec succès.',
+      buttons: ['OK']
+    });
+
+    await alert.present();
+  }
+  async afficherRemplir() {
+    const alert = await this.alertController.create({
+      header: 'Message',
+      message: 'Veuillez remplir tout les champs',
+      buttons: ['OK']
+    });
+
+    await alert.present();
+  }
+
+  async motdepasseinval() {
+    const alert = await this.alertController.create({
+      header: 'Message',
+      message: 'Mot de passe sont différent',
+      buttons: ['OK']
+    });
+
+    await alert.present();
+  }
+
+
+        ///////////////////
+
+loadNiveauList(): void {
+          this.niveauService.getNiveauList().subscribe(
+            (data) => {
+              this.niveaux = data;
+            },
+            (error) => {
+              console.error('Erreur lors du chargement de la liste des niveaux:', error);
+            }
+          );
+}
+
+async loadFilieres() {
+  try {
+    const niveauId = this.etudiantForm.get('niveau')?.value;
+    if (niveauId) {
+      const filieres = await this.filieresService.getFilieresByNiveau(niveauId).toPromise();
+      this.filieres = filieres;
+    }
+  } catch (error) {
+    console.error('Erreur lors du chargement des filières', error);
+
+    const alert = await this.alertController.create({
+      header: 'Erreur',
+      message: 'Une erreur s\'est produite lors du chargement des filières.',
+      buttons: ['OK']
+    });
+
+    await alert.present();
+  }
+}
+
+async loadClasses() {
+  try {
+    const filiereId = this.etudiantForm.get('filiere')?.value;
+    if (filiereId) {
+      const classes = await this.classesService.getClassesByFiliere(filiereId).toPromise();
+      this.classes = classes;
+    }
+  } catch (error) {
+    console.error('Erreur lors du chargement des classes', error);
+
+    const alert = await this.alertController.create({
+      header: 'Erreur',
+      message: 'Une erreur s\'est produite lors du chargement des classes.',
+      buttons: ['OK']
+    });
+
+    await alert.present();
+  }
+}
+
+
 
 }
--- src/app/service/etudiant.service.ts
+++ src/app/service/etudiant.service.ts
@@ -1,9 +1,60 @@
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Subject, catchError, map, tap } from 'rxjs';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class EtudiantService {
+  openSnackBar(arg0: string, arg1: string) {
+    throw new Error('Method not implemented.');
+  }
+  private updateEvent = new Subject<void>();
 
-  constructor() { }
+  update$ = this.updateEvent.asObservable();
+
+  private baseUrl = 'http://localhost:8080/etudiant/'; // Remplacez l'URL par celle de votre contrôleur Spring Boot
+
+  constructor(private http: HttpClient
+    ) { }
+  triggerUpdate() {
+    this.updateEvent.next();
+  }
+  creerEtudiant(etudiant: any): Observable<any> {
+    const url = 'http://localhost:8080/etudiant/podjocreate';
+
+    return this.http.post(url, etudiant, { responseType: 'text' })
+      .pipe(
+        map(response => {
+          try {
+            return JSON.parse(response); // Try to parse the response as JSON
+          } catch (error) {
+            return response; // If parsing fails, return the original response
+          }
+        }),
+        catchError(error => {
+          console.error('Erreur lors de la requête :', error);
+          throw error; // Rethrow the error after logging
+        })
+      );
+}
+
+
+
+
+  login(email: string, motDePasse: string): Observable<any> {
+    const headers = new HttpHeaders({ 'Content-Type': 'application/x-www-form-urlencoded' });
+
+    const body = new URLSearchParams();
+    body.set('email', email);
+    body.set('motDePasse', motDePasse);
+
+    return this.http.post(`${this.baseUrl}login`, body.toString(), { headers });
+  }
+
+  getEnseignantList():Observable<any> {
+    return this.http.get("http://localhost:8080/etudiant/list");
+    // console.log(this.getAdminList());
+  }
 }
